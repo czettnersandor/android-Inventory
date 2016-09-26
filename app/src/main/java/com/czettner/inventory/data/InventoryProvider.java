4fcbcd52dd232c939ae7f8c7681032e25baba8af
Sándor Czettner <sandor@czettner.hu>
2016-09-26 09:18:23 +0100

    InventoryProvider

---
+++ app/src/main/java/com/czettner/inventory/data/InventoryProvider.java
@@ -0,0 +1,58 @@
+package com.czettner.inventory.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+public class InventoryProvider extends ContentProvider {
+    public static final String LOG_TAG = InventoryProvider.class.getSimpleName();
+
+    private InventoryDbHelper mDbHelper;
+
+    public static final int STOCK = 100;
+    public static final int STOCK_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_STOCK, STOCK);
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_STOCK + "/#", STOCK_ID);
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new InventoryDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
